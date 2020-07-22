a4e9167339868e9d19bbd0656b684580e5b4d41a
Yazeed-Nazzal <yazeed_nazal@hotmail.com>
2020-07-22 16:39:57 +0300

    add animation on Features

--- js/js.js
+++ js/js.js
@@ -6,6 +6,26 @@ $(function () {
     console.log(`${win} + .... ${upper} +  ..... ${nav}`)
     // slider height
 
+
+    // window scroll event
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() >= 280){
+            $(".i1").removeClass("animate__fadeOut").addClass("animate__fadeInUp");
+            $(".i2").removeClass("animate__fadeOut").css("animation-delay","500ms").addClass("animate__fadeInUp");
+            $(".i3").removeClass("animate__fadeOut").css("animation-delay","1000ms").addClass("animate__fadeInUp");
+            $(".i4").removeClass("animate__fadeOut").css("animation-delay","1500ms").addClass("animate__fadeInUp");
+
+            // $(".Features-item").removeClass("animate__fadeOut").addClass("animate__fadeInUp");
+        }
+
+    });
+
+
+    // window scroll event
+
+
     $('.slider , .carousel-item').height(win - ( upper + nav ));
     //shuffle img
     $(".Featured-ul li").on("click",function () {
@@ -17,13 +37,11 @@ $(function () {
         else {
             $(".im").removeClass("animate__bounceIn").addClass("animate__bounceOut");
             $(".im").css("display","none");
-            $($(this).data("class")).removeClass("animate__bounceOut").addClass("animate__bounceIn")
+            $($(this).data("class")).removeClass("animate__bounceOut").addClass("animate__bounceIn");
             $($(this).data("class")).css("display","block");
 
         }
     });
-
-
     ////shuffle img
 
 
