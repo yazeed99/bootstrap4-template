f5be705bd5d3fc828c1eb68e52e0b6e89038be38
Yazeed-Nazzal <yazeed_nazal@hotmail.com>
2020-07-22 14:33:42 +0300

    add animate.css to project

--- js/js.js
+++ js/js.js
@@ -1,8 +1,28 @@
 $(function () {
+    // slider height
     var win = $(window).height();
     var upper = $('.upper-bar').innerHeight();
     var nav  = $('.navbar').innerHeight();
     console.log(`${win} + .... ${upper} +  ..... ${nav}`)
+    // slider height
 
     $('.slider , .carousel-item').height(win - ( upper + nav ));
+    //shuffle img
+    $(".Featured-ul li").on("click",function () {
+        $(this).addClass("li-active").siblings().removeClass("li-active");
+        if ($(this).data("class") == "all") {
+            $(".im").css("display", "block");
+        }
+        else {
+            $(".im").css("display", "none");
+            $($(this).data("class")).css("display", "block");
+
+
+        }
+    });
+
+
+    ////shuffle img
+
+
 });
